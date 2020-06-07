230476059e06af19c801361c8c13bdd3b8fb2c8f
Pro Grammar R <pro_grammar@outlook.com>
2020-06-07 02:47:41 -0500

    Essentially fully working custom palette editor. Import text doesn't appear to be working though.

--- customPalette/index.js
+++ customPalette/index.js
@@ -11,6 +11,8 @@
     monsters = require('../monsters');
   }
 
+  const HEX_FORMAT = 16;
+  const sectorsToShow = 4;
   const pixelsPerTile = 8;
   const tilesPerAxis = 16;
   const colorsPerPalette = 16;
@@ -35,7 +37,6 @@
   const elems = {};
   const monsterPalettes = {};
   let romArrayBuffer;
-  let floor;
   let colorStyles;
 
   let pageIsSetUp = false;
@@ -47,28 +48,61 @@
     return hexString;
   }
 
+  function fiveBitsToByte(fiveBits) {
+    return ensureHexStringHasTwoDigits((fiveBits << 3).toString(HEX_FORMAT));
+  }
+
+  function getMonsterId() {
+    return parseInt(elems.tileMonsterId.value);
+  }
+
+  function getPaletteType() {
+    return parseInt(elems.tileMonsterElement.value);
+  }
+
   function tileHandler(event) {
+    const classNameTokens = event.target.className.split('-');
+    const paletteColorIndex = parseInt(classNameTokens[classNameTokens.length - 1]);
+    for (let colorClickedLabelIndex = 1; colorClickedLabelIndex < colorsPerPalette; colorClickedLabelIndex++) {
+      elems.colorLastClickedLabels[colorClickedLabelIndex].hidden = colorClickedLabelIndex !== paletteColorIndex;
+    }
     event.preventDefault();
     event.stopPropagation();
   }
 
+  function transparencyColorHandler(event) {
+    //TODO this should use CSS
+    elems.spriteArea.style = 'background-color: ' + elems.transparencyColor.value;
+    //document.getElementById('palette-color-0').value = elems.transparencyColor.value;
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  function updatePaletteColorsToMatchPaletteType() {
+    for (let paletteColorIndex = 1; paletteColorIndex < colorsPerPalette; paletteColorIndex++) {
+      document.getElementById('palette-color-'+ paletteColorIndex.toString()).value = monsterPalettes[getMonsterId()][getPaletteType()][paletteColorIndex];
+    }
+  }
+
   function updateTilesToMatchPaletteAndPaletteType() {
-    const palettesByType = monsterPalettes[parseInt(elems.tileMonsterId.value)];
-    const palettes = palettesByType[elems.tileMonsterElement.value];
-    for (let paletteColorIndex = 0; paletteColorIndex < colorsPerPalette; paletteColorIndex++) {
+    const palettesByType = monsterPalettes[getMonsterId()];
+    const palettes = palettesByType[getPaletteType()];
+    for (let paletteColorIndex = 1; paletteColorIndex < colorsPerPalette; paletteColorIndex++) {
       colorStyles.deleteRule(paletteColorIndex);
       colorStyles.insertRule('.color-'+paletteColorIndex.toString()+' { background-color: '+ palettes[paletteColorIndex] + '}', paletteColorIndex);
     }
   }
 
   function makePixelsMatchChosenMonster() {
-    const monsterId = parseInt(elems.tileMonsterId.value);
+    const monsterId = getMonsterId();
     const spritesLocation = (firstMonsterSector + (monsterId - 1) * (animationDataSectors + spriteDataSectors + paletteDataSectors) + animationDataSectors) * constants.sectorSize + constants.headerSize;
-    const sprites = new DataView(romArrayBuffer, spritesLocation, constants.sectorDataSize);
-    for (let offset = 0; offset < constants.sectorDataSize; offset++) {
-      let value = sprites.getUint8(offset);
-      tiles[offset*2].className = 'floor-tile color-' + (value & 0x0f).toString();
-      tiles[offset*2 + 1].className = 'floor-tile color-' + (value >>> 4).toString();
+    const sprites = new DataView(romArrayBuffer, spritesLocation, sectorsToShow * constants.sectorSize);
+    for (let spriteSet = 0; spriteSet < sectorsToShow; spriteSet++) {
+      for (let offset = 0; offset < constants.sectorDataSize; offset++) {
+        let value = sprites.getUint8(spriteSet * constants.sectorSize + offset);
+        tiles[(spriteSet * constants.sectorDataSize + offset)*2].className = 'floor-tile color-' + (value & 0x0f).toString();
+        tiles[(spriteSet * constants.sectorDataSize + offset)*2 + 1].className = 'floor-tile color-' + (value >>> 4).toString();
+      }
     }
     if (!monsterPalettes[monsterId]) {
       const paletteData = new DataView(romArrayBuffer, spritesLocation + spriteDataSectors * constants.sectorSize, constants.sectorDataSize);
@@ -76,7 +110,7 @@
       for (let paletteType = 0; paletteType < numPaletteTypes; paletteType++) {
         const palette = [];
         let paletteColorIndex = 0;
-        if (paletteData.getUint16(paletteColorIndex++ * 2 + paletteType * 0x80, true) !== 0) {
+        if (paletteData.getUint16(paletteColorIndex * 2 + paletteType * 0x80, true) !== 0) {
           alert('color index 0 is not transparent');
         }
         for (; paletteColorIndex < colorsPerPalette; paletteColorIndex++) {
@@ -84,17 +118,34 @@
           const b = (read & 0x7fff) >>> 10;
           const g = (read & 0x03ff) >>> 5;
           const r = (read & 0x001f);
-          palette[paletteColorIndex] = '#' + (r << 3).toString(16) + (g << 3).toString(16) + (b << 3).toString(16);
+          palette[paletteColorIndex] = '#' + fiveBitsToByte(r) + fiveBitsToByte(g) + fiveBitsToByte(b);
         }
         monsterPalette[paletteType] = palette;
       }
       monsterPalettes[monsterId] = monsterPalette;
     }
+    updatePaletteColorsToMatchPaletteType();
     updateTilesToMatchPaletteAndPaletteType(); 
   }
 
   function tileMonsterIdHandler(event) {
     makePixelsMatchChosenMonster();
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  function tileMonsterElementHandler(event) {
+    updatePaletteColorsToMatchPaletteType();
+    updateTilesToMatchPaletteAndPaletteType();
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  function paletteColorHandler(event) {
+    const clicked = event.target;
+    const clickedId = clicked.id.split('-')[2];
+    monsterPalettes[getMonsterId()][getPaletteType()][clickedId] = clicked.value;
+    updateTilesToMatchPaletteAndPaletteType();
     event.preventDefault();
     event.stopPropagation();
   }
@@ -126,8 +177,9 @@
       reader.onload = function() {
         const importExportField = document.getElementById('import-export');
         importExportField.value = this.result;
-        floor = JSON.parse(importExportField.value);
-        processImport();
+        monsterPalettes = JSON.parse(importExportField.value);
+        updatePaletteColorsToMatchPaletteType();
+        updateTilesToMatchPaletteAndPaletteType();
       }
       reader.readAsText(elems.importFile.files[0]);
     }
@@ -139,8 +191,9 @@
     //catch enter being pressed in other fields which triggers this, for some reason
     if (event.detail !== 0) {
       const importExportField = document.getElementById('import-export');
-      floor = JSON.parse(importExportField.value);
-      processImport();
+      monsterPalettes = JSON.parse(importExportField.value);
+      updatePaletteColorsToMatchPaletteType();
+      updateTilesToMatchPaletteAndPaletteType();
     }
     event.preventDefault();
     event.stopPropagation();
@@ -148,18 +201,18 @@
 
   function exportTextHandler(event) {
     const importExportTextbox = document.getElementById('import-export');
-    importExportTextbox.value = JSON.stringify(floor);
+    importExportTextbox.value = JSON.stringify(monsterPalettes);
     event.preventDefault();
     event.stopPropagation();
   }
 
   function exportFileHandler(event) {
-    const urlDownload = URL.createObjectURL(new Blob([JSON.stringify(floor)], {
+    const urlDownload = URL.createObjectURL(new Blob([JSON.stringify(monsterPalettes)], {
       type: 'text/json; charset=UTF-8',
     }));
     const downloadFileElem = document.getElementById('download-file');
     downloadFileElem.href = urlDownload;
-    downloadFileElem.download = 'customFloor.json';
+    downloadFileElem.download = 'customPalettes.json';
     downloadFileElem.click();
     URL.revokeObjectURL(urlDownload);
     event.preventDefault();
@@ -178,7 +231,18 @@
 
   function setUpPage() {
     document.onkeypress = keyPressHandler;
-    const spriteArea = document.getElementById('sprite-area');
+
+    elems.spriteArea = document.getElementById('sprite-area');
+    elems.importBinFile = document.getElementById('import-bin-file');
+    elems.importFile = document.getElementById('import-file');
+    elems.importText = document.getElementById('import-text');
+    elems.exportText = document.getElementById('export-text');
+    elems.exportFile = document.getElementById('export-file');
+    elems.transparencyColor = document.getElementById('transparency-color');
+    elems.tileMonsterId = document.getElementById('tile-monster-id');
+    elems.tileMonsterElement = document.getElementById('tile-monster-element');
+    elems.paletteColors = document.getElementById('palette-colors');
+
     tileNumber = 0;
     const sheet = document.createElement('style')
     sheet.title = 'color-styles';
@@ -190,7 +254,7 @@
       }
     }
     for (let styleIndex = 0; styleIndex < 16; styleIndex++) {
-      colorStyles.insertRule('.color-'+styleIndex.toString()+' {background-color: black;}', styleIndex);
+      colorStyles.insertRule('.color-'+styleIndex.toString()+' {background-color: #778899;}', styleIndex);
     }
     
     for (row = 0; row < pixelsPerAxis; row++) {
@@ -210,35 +274,40 @@
         tileRow.appendChild(tile);
         tiles[tileNumber++] = tile;
       }
-      spriteArea.appendChild(tileRow);
+      elems.spriteArea.appendChild(tileRow);
     }
-
-    elems.importBinFile = document.getElementById('import-bin-file');
-    elems.importFile = document.getElementById('import-file');
-    elems.importText = document.getElementById('import-text');
-    elems.exportText = document.getElementById('export-text');
-    elems.exportFile = document.getElementById('export-file');
-    elems.transparencyColor = document.getElementById('export-file');
-    elems.tileMonsterId = document.getElementById('tile-monster-id');
-    elems.tileMonsterElement = document.getElementById('tile-monster-element');
-    elems.paletteColors = document.getElementById('palette-colors');
 
     elems.importBinFile.addEventListener('change', importBinFileHandler);
     elems.importFile.addEventListener('change', importFileHandler);
     elems.importText.addEventListener('click', importTextHandler);
     elems.exportText.addEventListener('click', exportTextHandler);
     elems.exportFile.addEventListener('click', exportFileHandler);
+    elems.transparencyColor.addEventListener('input', transparencyColorHandler, false);
+    elems.transparencyColor.addEventListener('change', transparencyColorHandler, false);
     elems.tileMonsterId.addEventListener('change', tileMonsterIdHandler);
+    elems.tileMonsterElement.addEventListener('change', tileMonsterElementHandler);
 
+    elems.colorLastClickedLabels = [document.getElementById('palette-color-note')];
     for (let paletteColorIndex = 1; paletteColorIndex < colorsPerPalette; paletteColorIndex++) {
-      const elementId = 'color-' + paletteColorIndex.toString();
+      const div = document.createElement('div');
+      div.className = 'pure-u-1 hflow';
+      const elementId = 'palette-color-' + paletteColorIndex.toString();
       const label = document.createElement('label');
       label.for = elementId;
       label.innerText = paletteColorIndex.toString();
-      elems.paletteColors.appendChild(label);
+      div.appendChild(label);
       const input = document.createElement('input');
       input.type = 'color';
-      elems.paletteColors.appendChild(input);
+      input.id = elementId;
+      input.addEventListener('change', paletteColorHandler, false);
+      input.addEventListener('input', paletteColorHandler, false);
+      div.appendChild(input);
+      const lastClickedLabel = document.createElement('div');
+      lastClickedLabel.innerText = 'Clicked this color last'
+      lastClickedLabel.hidden = true;
+      div.appendChild(lastClickedLabel);
+      elems.colorLastClickedLabels[paletteColorIndex] = lastClickedLabel;
+      elems.paletteColors.appendChild(div);
     }
 
     monsters.allMonsters.forEach(function(monster) {
